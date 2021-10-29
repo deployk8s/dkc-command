602fdeed238e5d008eb69e4c071d67e09e31b05a
hippopos <wenchang_cui@bizconf.cn>
2021-10-29 17:01:39 +0800

    update

--- src/prepare/table.go
+++ src/prepare/table.go
@@ -26,7 +26,7 @@ func embarkData(t *src.Table, tds []inventory.Host) {
 		} else {
 			log.Log.Error(err.Error())
 		}
-		var rma, rn, rsg, rom string
+		var rma, rn, rom string
 
 		if row.IsMaster {
 			rma = "yes"
@@ -37,21 +37,13 @@ func embarkData(t *src.Table, tds []inventory.Host) {
 		if row.IsOm {
 			rom = "yes"
 		}
-		//if row.IsTracking {
-		//	rtra = "yes"
-		//}
-		if row.IsSg {
-			rsg = "yes"
-		}
 
 		r := []*simpletable.Cell{
 			{Align: simpletable.AlignCenter, Text: row.Hostname},
 			{Align: simpletable.AlignCenter, Text: rma},
 			{Align: simpletable.AlignCenter, Text: rn},
-			{Align: simpletable.AlignCenter, Text: row.IsMongo},
 			{Align: simpletable.AlignCenter, Text: rom},
 			//{Text: rtra},
-			{Align: simpletable.AlignCenter, Text: rsg},
 			{Align: simpletable.AlignCenter, Text: row.Ip},
 			{Align: simpletable.AlignCenter, Text: row.Arch},
 		}
